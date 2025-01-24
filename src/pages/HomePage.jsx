d89c741c2934345280cc590297b4954db2d6ead5
Soumyabrata Sarkar <sarkar.soumyabrata2@gmail.com>
2025-01-24 22:06:00 +0530

    Updated home page UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,21 +10,21 @@ const HomePage = () => {
     <div className="center flex-grow px-7">
       <IconInfoBox
         config={{
-          icon: <MessageSquare size={70} strokeWidth={2} color="#6366f1" />,
+          icon: <MessageSquare size={70} strokeWidth={0} fill="#007bff" />,
         }}
       />
 
-      <div className="flex flex-col items-center text-center mt-10 text-2xl md:text-3xl lg:text-5xl">
-        <span className="font-thin">
+      <div className="flex flex-col items-center text-center mt-7">
+        <span className="text-xl font-thin md:text-3xl">
           {process.env.REACT_APP_TAGLINE_1 || "Unlock the Future of Learning"}
         </span>
-        <span className="font-bold mt-5">
+        <span className="text-3xl font-bold mt-5 md:text-5xl">
           {process.env.REACT_APP_TAGLINE_2 ||
             "AI-Powered Guidance to Boost Your Tech Skills"}
         </span>
 
         <div
-          className="flex flex-row mt-10 text-indigo-500 cursor-pointer lg:text-3xl hover:animate-pulse"
+          className="flex flex-row mt-10 text-[#007bff] cursor-pointer text-xl md:text-3xl hover:animate-pulse"
           onClick={() => navigator("/chat")}
         >
           <div className="v-center mr-2">
